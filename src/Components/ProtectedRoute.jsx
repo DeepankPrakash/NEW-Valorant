44dc41f468e81dcc4454cd1fb0d1c439f65ab88f
Deepank Prakash <115063649+DeepankPrakash@users.noreply.github.com>
2026-02-16 11:58:47 +0530

    Enhance ProtectedRoute with login alert
    Added alert for unauthorized access attempts.

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function ProtectedRoute({ children }) {
 
   const user = useSelector((state) => state.auth.user);
+  const location = useLocation();
 
   if (!user) {
+
+    if (location.pathname !== "/login") {
+      alert("Please log in to access this page.");
+    }
+
     return <Navigate to="/login" replace />;
   }
 
