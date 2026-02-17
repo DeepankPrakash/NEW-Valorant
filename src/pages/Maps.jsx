905ebd0085fff2919f1c310463b15a6cace8af39
Deepank Prakash <115063649+DeepankPrakash@users.noreply.github.com>
2026-02-17 09:52:12 +0530

    Update Maps component layout and styling
    Refactor Maps component to use a background image and Stack layout.

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
-import { Container, Typography, Grid, Box } from "@mui/material";
-import {maps} from "../data/maps";
+// import { useState } from "react";
+import { Container, Typography, Grid, Box, Stack  } from "@mui/material";
+import MapImage from "../assets/mapimage.jpg";
+import { maps } from "../data/maps";
 
 
 function Maps(){
-    const [maplist] = useState(maps);
+    // const [maplist] = useState(maps);
 
-    // useState(() => {
-    //     console.log(maplist);
+    // // useState(() => {
+    // //     console.log(maplist);
 
-    // });
-
-
+    // // });
+ 
     return(
-        <Container sx={{ mt: 6 }}>
-            <Typography variant = 'h3' mb ={4} > Maps </Typography>
+        
+        <Box sx={{ mt: 6, backgroundImage: `url(${MapImage})`, backgroundSize: "cover", backgroundPosition: "center", minHeight: "100vh" }}>
+            <Container>      
+                 <Typography variant = 'h3' mb ={4} > Maps </Typography>
+            <Stack spacing={3} sx={{backgroundColor: "rgba(0, 0, 0, 0.8)", p: 3, borderRadius: 2}}>
             <Grid container spacing={3}>
-                {maplist.map((map) => (
+                {maps.map((map) => (
                     <Grid item xs={12} sm={6} md={4} key={map.id}>
-                        <Box sx={{border: "1px solid #ccc", borderRadius: 2, p: 2, height: "100%"}}>
+                        <Box sx={{border: "1px solid #b40606", borderRadius: 2, p: 2, height: "100%"}}>
                             <Typography variant="h5" fontWeight={700}>{map.name}</Typography>
                             <Typography variant="subtitle1" color="text.secondary">{map.location}</Typography>
                             <Typography variant="body2" color="text.secondary" mt={1}>{map.description}</Typography>
@@ -27,7 +30,10 @@ function Maps(){
                   </Grid>
                 ))}
             </Grid>
-        </Container>
+            </Stack>
+            </Container>
+     
+        </Box>
         
     )
 }
