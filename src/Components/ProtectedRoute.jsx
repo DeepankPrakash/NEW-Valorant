b2dfc86b87e7ac5cc2a557fe5d7efd06b26ed7d7
Deepank Prakash <115063649+DeepankPrakash@users.noreply.github.com>
2026-02-17 09:49:13 +0530

    Refactor ProtectedRoute component
    Refactor ProtectedRoute to conditionally show alert when accessing protected routes.

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,3 +1,20 @@
+// import { Navigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+
+// function ProtectedRoute({ children }) {
+
+//   const user = useSelector((state) => state.auth.user);
+
+//   if (!user) {
+
+//     alert("Please log in to access this page.");
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return children;
+// }
+
+// export default ProtectedRoute;
 import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -8,6 +25,7 @@ function ProtectedRoute({ children }) {
 
   if (!user) {
 
+    // Only show alert if user is trying to access protected route manually
     if (location.pathname !== "/login") {
       alert("Please log in to access this page.");
     }
