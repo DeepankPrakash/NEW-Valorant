11c3825d046e0f0c4925247aeb42bd71463a985a
Deepank Prakash <115063649+DeepankPrakash@users.noreply.github.com>
2026-02-17 09:52:34 +0530

    Implement login validation and error display
    Add error handling for login credentials.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,51 @@
 import { TextField, Typography, Box, Button } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../Utils/auth";
+import { login } from "../redux/authSlice.js";
 import { useDispatch } from "react-redux";
+
 
 function Login(){
 
     const [id, setId] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleSubmit = () =>{
-        // e.preventDefault();
-        // const success = login(id, password);
-        // if(success){
-        //     navigate("/");
-        // }
-        dispatch(login({
-            id, password, token : "dummy-token"
-        }))
+
+       if (id === "admin" && password === "admin123"){
+        dispatch(login({id:"admin", username: "Admin User", token: "dummy-token"}));
         navigate("/");
-    }
+
+       }else{
+        setError("Invalid credentials.");
+      }
+    };
 
     return (
+
+        <>
         <Box sx={{p:5, maxWidth: 400, mx : 'auto'}}>
             <Typography variant ='h4'> Login </Typography>
+
+            {error &&
+             <Typography color="error">{error}</Typography>}
             <TextField label="User ID "
-            fullWidth margin="normal" onChange={(e)=> setId(e.target.value)}
+            fullWidth margin="normal" value={id} onChange={(e)=>{
+                setId(e.target.value);
+                setError("");
+            } 
+        }
             ></TextField>
             <TextField label="Password"
-            fullWidth margin="normal" type="password" onChange={(e)=> setPassword(e.target.value)}
+            fullWidth margin="normal" type="password" value={password} onChange={(e)=> {setPassword(e.target.value); setError("");}}
             ></TextField>
 
             <Button variant='contained' onClick={handleSubmit}>Login</Button>
 
         </Box>
-
+        </>
     )
 }
 
