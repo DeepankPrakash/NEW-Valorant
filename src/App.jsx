366022bb1ca96b98c3fa64619f963f752bfbabb7
Deepank Prakash <115063649+DeepankPrakash@users.noreply.github.com>
2026-02-18 11:41:59 +0530

    Refactor App component with new routes and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,3 @@
-// import { Route, Routes } from 'react-router-dom';
-// import MainLayout from "./layout/MainLayout";
-// import Login from './pages/Login';
-// import ProtectedRoute from './Components/ProtectedRoute';
-// import Home from './pages/Home';
-// import Agents from './pages/Agents';
-// import GameModes from './pages/GameModes';
-// import Ranks from './pages/Ranks';
-// import Maps from './pages/Maps';
-
-// function App(){
-//   return(
-//     <Routes>
-//       <Route element={<MainLayout />}>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/agents" element={<Agents />} />
-//         <Route path="/gamemodes" element={<GameModes />} />
-//         <Route path="/ranks" element={<Ranks />} />
-//         <Route path= "/maps" element={<Maps />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path ='/' element = {
-//           <ProtectedRoute>
-//             <MainLayout />
-//           </ProtectedRoute>
-//         }></Route>
-//       </Route>
-//     </Routes>
-//   )
-// }
-
-// export default App;
-
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Login from "./pages/Login";
@@ -39,22 +7,33 @@ import Agents from "./pages/Agents";
 import GameModes from "./pages/GameModes";
 import Ranks from "./pages/Ranks";
 import Maps from "./pages/Maps";
+import AppHeader from "./Components/AppHeader";
+// import { Toolbar } from "@mui/material";
+import NotFound from "./pages/NotFound";
+import AgentDetails from "./pages/AgentDetails";
 
 function App() {
   return (
-    <Routes>
 
+    <>
+    
+    <AppHeader />
+    <Routes>
+      
       <Route path="/login" element={<Login />} />
 
       <Route path="/" element={<ProtectedRoute><MainLayout /></ProtectedRoute>}>
         <Route index element={<Home />} />
         <Route path="agents" element={<Agents />} />
+        <Route path="agents/:agentName" element={<AgentDetails />} />
         <Route path="gamemodes" element={<GameModes />} />
         <Route path="ranks" element={<Ranks />} />
         <Route path="maps" element={<Maps />} />
+        <Route path = "*" element={<NotFound />} />
       </Route>
 
     </Routes>
+    </>
   );
 }
 
